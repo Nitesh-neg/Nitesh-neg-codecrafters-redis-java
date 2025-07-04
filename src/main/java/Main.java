91bea069a09563adf79f88cdc628021f3154aec6
Nitesh <kaindalnitesh909@gmail.com>
2025-07-04 19:40:00 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -32,21 +37,33 @@ public class Main {
             clientSocket; // This automatically closes the socket at the end
             OutputStream outputStream = clientSocket.getOutputStream()
         ) {
-            byte[] input = new byte[1024];
 
             while (true) {
-                int bytesRead = clientSocket.getInputStream().read(input);
-                if (bytesRead == -1) {
-                    break; // client closed connection
-                }
-
-                String str = new String(input, 0, bytesRead).trim();
-                System.out.println("Received: " + str);
-
-                outputStream.write("+PONG\r\n".getBytes());
+                List<String> command = parseRESP(clientSocket.getInputStream());
+                      if (command.get(0).equalsIgnoreCase("PING")) {
+                          System.out.println("+PONG\r\n".getBytes());
+                      }
             }
         } catch (IOException e) {
             System.out.println("Client disconnected or error: " + e.getMessage());
         }
     }
+
+          public static List<String> parseRESP(InputStream in) throws IOException {
+          BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+          List<String> result = new ArrayList<>();
+
+          String line = reader.readLine(); // *2
+          if (line == null || !line.startsWith("*")) return result;
+
+          int numArgs = Integer.parseInt(line.substring(1));
+          for (int i = 0; i < numArgs; i++) {
+              reader.readLine(); // Read $<length>, ignore for now
+              String arg = reader.readLine(); // actual string
+              result.add(arg);
+          }
+
+          return result;
+      }
+
 }
